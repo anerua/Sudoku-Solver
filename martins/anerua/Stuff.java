4f1559f87f83c4b94ee5e5fa975bf3584c77bf18
Martins Anerua <aneruamartins@gmail.com>
2020-11-18 18:03:10 +0100

    separate cli launcher and puzzle solver classes

--- martins/anerua/Stuff.java
+++ martins/anerua/Stuff.java
@@ -1,8 +1,6 @@
 package martins.anerua;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
 
 //======================================================================
@@ -14,40 +12,21 @@ import java.util.Set;
 
 public class Stuff {
 
-	public String printBoard(String[] board) {
-		if (board.length == 1) {
-			return "Puzzle has no solution";
-		}
-		String output = ".=============================.\n";
-		for (int i = 0; i < board.length; i++) {
-			output += "| ";
-			for (int j = 0; j < board[i].length(); j++) {
-				output += board[i].charAt(j) + " ";
-				output += (j%3 == 2) ? "| " : " ";
-			}
-			output += (i%3 == 2 && i != 8) ? "\n: ------- + ------- + ------- :\n" : "\n";
-		}
-		output += "*=============================*";
-		return output;
-	}
-
 	private String boxString(String[] board, int focusRow, int focusCol) {
 		String box = "";
 		int initRow = (focusRow / 3) * 3;
 		int initCol = (focusCol / 3) * 3;
 		for (int i = initRow; i < initRow + 3; i++) {
-			for (int j = initCol; j < initCol + 3; j++) {
+			for (int j = initCol; j < initCol + 3; j++)
 				box += board[i].charAt(j);
-			}
 		}
 		return box;
 	}
 
 	private String colString(String[] board, int focusCol) {
 		String col = "";
-		for (String row : board) {
+		for (String row : board)
 			col += row.charAt(focusCol);
-		}
 		return col;
 	}
 
@@ -63,7 +42,6 @@ public class Stuff {
 		if (rowCount > 1) {
 			return false;
 		}
-
 		// check column
 		String col = colString(board, focusCol);
 		int colCount = 0;
@@ -74,7 +52,6 @@ public class Stuff {
 		if (colCount > 1) {
 			return false;
 		}
-
 		// check box
 		String box = boxString(board, focusRow, focusCol);
 		int boxCount = 0;
@@ -85,21 +62,18 @@ public class Stuff {
 		if (boxCount > 1) {
 			return false;
 		}
-
-		return true;
+		return true; // puzzle satisfies constraints
 	}
 
 	private String writeCell(String row, String candidate, int focusCol) {
 		String newRow = "";
-		for (int i = 0; i < row.length(); i++) {
+		for (int i = 0; i < row.length(); i++)
 			newRow += (i == focusCol) ? candidate : row.charAt(i);
-		}
 		return newRow;
 	}
 
 	public String[] DFS(String[] board) {
 		int focusRow = 81, focusCol = 81;
-
 		for (int i = 0; i < 9; i++) {
 			if (board[i].contains("0")) {
 				focusRow = i;
@@ -107,7 +81,6 @@ public class Stuff {
 				break;
 			}
 		}
-
 		if (focusRow == 81 && focusCol == 81) {
 			return board; // puzzle is solved
 		}
@@ -118,12 +91,10 @@ public class Stuff {
 			tempBoard[focusRow] = writeCell(tempBoard[focusRow], candidate, focusCol);
 			if (checkConstraint(tempBoard, focusRow, focusCol)) {
 				String[] newBoard = DFS(tempBoard);
-				if (newBoard.length != 1) {
+				if (newBoard.length != 1)
 					return newBoard;
-				}
 			}
 		}
-
 		return new String[] { "0" };
 	}
 	
@@ -140,77 +111,11 @@ public class Stuff {
 					++filledSquares;
 					filledDigits.add(Character.toString(board[i].charAt(j)));
 				}
-				if (filledDigits.size() >= 8 && filledSquares >= 17) {
+				if (filledDigits.size() >= 8 && filledSquares >= 17)
 					return true;
-				}
 			}
 		}
 		return false;
-	}
-	
-	private static boolean verifyInput(String inputRow) {
-		if (inputRow.length() != 9) {
-			return false;
-		}
-		String[] valids = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-		for (int i = 0; i < inputRow.length(); i++) {
-			if (!Arrays.asList(valids).contains(Character.toString(inputRow.charAt(i)))) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public static String[] getInput() {
-		String[] board = new String[9];
-		Scanner scanner = new Scanner(System.in);
-		for (int i = 0; i < 9; i++) {
-			boolean correct = false;
-			while (!correct) {
-				System.out.print("Row " + (i+1) + ": ");
-				String inputRow = scanner.nextLine().strip();
-				if (verifyInput(inputRow)) {
-					board[i] = inputRow;
-					correct = true;
-				} else {
-					System.out.println("Invalid input!");
-				}
-			}
-		}
-		scanner.close();
-		return board;
-	}
-	
-	public static void startMessage() {
-		System.out.println("================================================");
-		System.out.println("                 Sudoku Solver                  ");
-		System.out.println("================================================");
-		System.out.println();
-		System.out.println("Enter the values of cells in each row.");
-		System.out.println("Use 0 to represent an empty cell");
-		System.out.println("Do not add space in between cell values.");
-		System.out.println("A sample row with only one empty cell would be: 012345678");
-		System.out.println();
-	}
-
-	public static void main(String[] args) {
-		startMessage();
-		String[] board = getInput();
-		System.out.println("Verifying...");
-		if (isSolvable(board)) {
-			System.out.println("Solving...");
-			long startTime = System.nanoTime();
-			Stuff stuff = new Stuff();
-			String[] solution = stuff.DFS(board);
-			long endTime = System.nanoTime();
-			float programTime = (float) (endTime - startTime) / 1000000000;
-			System.out.println("Solution:");
-			System.out.println(stuff.printBoard(solution));
-			System.out.println("Program took: " + programTime + " seconds.");
-		} else {
-			System.out.println("Puzzle either has duplicate solutions or is unsolvable");
-		}
-
 	}
 
 }
