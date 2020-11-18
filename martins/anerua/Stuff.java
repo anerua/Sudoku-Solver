f8b34162f409b83fd76cc0c31c9a1e903b9a8840
Martins Anerua <aneruamartins@gmail.com>
2020-11-18 15:21:59 +0100

    solve user defined game

--- martins/anerua/Stuff.java
+++ martins/anerua/Stuff.java
@@ -1,6 +1,8 @@
 package martins.anerua;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 //======================================================================
@@ -16,13 +18,24 @@ public class Stuff {
 		if (board.length == 1) {
 			return "Puzzle has no solution";
 		}
-		String output = "";
-		for (String row : board) {
-			for (int i = 0; i < row.length(); i++) {
-				output += row.charAt(i) + " " + "|";
+		String output = ".=============================.\n";
+		for (int i = 0; i < board.length; i++) {
+			output += "| ";
+			for (int j = 0; j < board[i].length(); j++) {
+				output += board[i].charAt(j) + " ";
+				if (j%3 == 2) {
+					output += "| ";
+				} else {
+					output += " ";
+				}
 			}
-			output += "\n---------------------------\n";
+			if (i%3 == 2 && i != 8) {
+				output += "\n: ------- + ------- + ------- :\n";
+			} else {
+				output += "\n";
+			}
 		}
+		output += "*=============================*";
 		return output;
 	}
 
@@ -116,9 +129,7 @@ public class Stuff {
 			tempBoard[focusRow] = writeCell(tempBoard[focusRow], candidate, focusCol);
 			if (checkConstraint(tempBoard, focusRow, focusCol)) {
 				String[] newBoard = DFS(tempBoard);
-				if (newBoard.length == 1) {
-					continue;
-				} else {
+				if (newBoard.length != 1) {
 					return newBoard;
 				}
 			}
@@ -143,6 +154,51 @@ public class Stuff {
 		}
 		return false;
 	}
+	
+	private static boolean verifyInput(String inputRow) {
+		if (inputRow.length() != 9) {
+			return false;
+		}
+		String[] valids = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+		for (int i = 0; i < inputRow.length(); i++) {
+			if (!Arrays.asList(valids).contains(Character.toString(inputRow.charAt(i)))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static String[] getInput() {
+		String[] board = new String[9];
+		Scanner scanner = new Scanner(System.in);
+		for (int i = 0; i < 9; i++) {
+			boolean correct = false;
+			while (!correct) {
+				System.out.print("Row " + (i+1) + ": ");
+				String inputRow = scanner.nextLine().strip();
+				if (verifyInput(inputRow)) {
+					board[i] = inputRow;
+					correct = true;
+				} else {
+					System.out.println("Invalid input!");
+				}
+			}
+		}
+		scanner.close();
+		return board;
+	}
+	
+	public static void startMessage() {
+		System.out.println("================================================");
+		System.out.println("                 Sudoku Solver                  ");
+		System.out.println("================================================");
+		System.out.println();
+		System.out.println("Enter the values of cells in each row.");
+		System.out.println("Use 0 to represent an empty cell");
+		System.out.println("Do not add space in between cell values.");
+		System.out.println("A sample row with only one empty cell would be: 012345678");
+		System.out.println();
+	}
 
 	public static void main(String[] args) {
 		String[] test1 = { "001900070", "780001090", "004030805", "060000009", "008020100", "100000060", "906040200",
@@ -166,8 +222,10 @@ public class Stuff {
 		String[] test10 = { "000003540", "010060002", "309002007", "240030000", "006000300", "000010024", "700300406",
 				"500020090", "031500000" }; // Solo 3x3 Unreasonable
 		
-		String[] board = test10;
+		startMessage();
+		String[] board = getInput();
 		if (isSolvable(board)) {
+			System.out.println("Solving...");
 			long startTime = System.nanoTime();
 			Stuff stuff = new Stuff();
 			String[] solution = stuff.DFS(board);
